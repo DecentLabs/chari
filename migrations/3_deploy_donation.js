698d498fbb84a360a9dba4495f99571795a62019
Viktor Szathmáry <phraktle@gmail.com>
2019-06-28 15:44:41 +0200

    cleanup

--- migrations/3_deploy_donation.js
+++
@@ -1,5 +0,0 @@
-var DonationMatching = artifacts.require("./DonationMatching.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(DonationMatching);
-};
---
+++ test/donationmatching.js
@@ -0,0 +1,9 @@
+const DonationMatching = artifacts.require("./DonationMatching.sol");
+
+contract("DonationMatching", accounts => {
+  it("should deploy", async () => {
+
+  	console.log(DonationMatching);
+
+  });
+});
