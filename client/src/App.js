fda57beabff5b02e4f11f048d454c4c43c2887b5
juli_e <julia.ero@euedge.com>
2019-06-28 12:48:07 +0200

    create contract button

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,71 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./utils/getWeb3";
+import React, { Component } from 'react';
+import SimpleStorageContract from './contracts/SimpleStorage.json';
+import getWeb3 from './utils/getWeb3';
+import DeployButton from './components/DeployButton';
 
-import "./App.css";
+import './App.css';
+
+
+const DURATION = 7 * 24 * 60 * 60
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+    state = {
+        storageValue: 0,
+        web3: null,
+        accounts: null,
+        contract: null,
+        recipient: '0xFdfd7F1F68c22b572DcBEB831425741d11D728bb',
+        expDate: Math.floor(Date.now() / 1000) + DURATION
+    };
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+    componentDidMount = async () => {
+        try {
+            // Get network provider and web3 instance.
+            const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+            // Use web3 to get the user's accounts.
+            const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+            // Get the contract instance.
+            const networkId = await web3.eth.net.getId();
+            // Set web3, accounts, and contract to the state, and then proceed with an
+            // example of interacting with the contract's methods.
+            this.setState({web3, accounts, networkId});
+        } catch (error) {
+            // Catch any errors for any of the above operations.
+            alert(
+                `Failed to load web3, accounts, or contract. Check console for details.`,
+            );
+            console.error(error);
+        }
+    };
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
+    render () {
+        if (!this.state.web3) {
+            return <div>Loading Web3, accounts, and contract...</div>;
+        }
+        return (
+            <div className="App">
+                <h1>Good to Go!</h1>
+                <p>Your Truffle Box is installed and ready.</p>
+                <h2>Smart Contract Example</h2>
+                <p>
+                    If your contracts compiled and migrated successfully, below will show
+                    a stored value of 5 (by default).
+                </p>
+                <p>
+                    Try changing the value stored on <strong>line 40</strong> of App.js.
+                </p>
+                <div>The stored value is: {this.state.storageValue}</div>
+                <DeployButton
+                    web3={this.state.web3}
+                    account={this.state.accounts[0]}
+                    recipient={this.state.recipient}
+                    expiration={this.state.expDate}
+                />
+            </div>
+        );
     }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
-  }
 }
 
 export default App;
---
+++ client/src/components/DeployButton.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import DonationMatchingJSON from '../contracts/DonationMatching.json'
+
+export default class DeployButton extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {contract: null}
+        this.deploy = this.deploy.bind(this);
+    }
+
+    deploy() {
+        const abi = DonationMatchingJSON.abi;
+        const byteCode = DonationMatchingJSON.deployedBytecode;
+        const web3 = this.props.web3;
+        const contract = new web3.eth.Contract(abi)
+        const tx = contract.deploy({data: byteCode, arguments: [this.props.recipient, this.props.expiration]}).send({
+            from: this.props.account,
+            gas: 2000000
+        });
+        tx.then((newContractInstance) => {
+            console.log(newContractInstance)
+            this.setState({
+                contract: newContractInstance
+            })
+        })
+    }
+
+    render() {
+        const {contract} = this.state
+
+        return (<div>
+            <button onClick={this.deploy}>Deploy Contract</button>
+            {contract && (<p>
+                {contract.options.address}
+            </p>)}
+        </div>)
+    }
+}
