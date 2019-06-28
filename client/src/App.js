6c264f7a904e6e11bdcff53fd93b664ff0f03720
brigittaforrai <forraibrigi@gmail.com>
2019-06-28 11:53:17 +0200

    brigi init

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
 
+import Navigation from './components/navigation.js'
+
 import "./App.css";
 
 class App extends Component {
@@ -54,17 +56,7 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <Navigation></Navigation>
       </div>
     );
   }
---
+++ client/src/components/navigation.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './../styles/navigation.module.css'
+
+console.log(styles, "styles");
+
+export default function Navigation () {
+  return <div className={styles.navigation}>navigation</div>
+}
