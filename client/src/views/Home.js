65e6be0956d4f61c1e0a81ffb14b882ed7b22e98
brigittaforrai <forraibrigi@gmail.com>
2019-07-01 17:01:33 +0200

    home content + create new btn

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import styles from './../styles/Home.module.css'
+import {NavLink} from "react-router-dom"
+
+import Button from './../components/button.js'
 
 export default function Home () {
   return (
     <div className={styles.home}>
       <header>
-        <h1>Donation Matching</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, ut labore et dolore magna aliqua.</p>
-        <p>Condimentum lacinia quis vel eros donec ac odio.  <br/>Enim lobortis scelerisque fermentum dui faucibus in ornare. </p>
+        <h1>A new class of potential matching sponsors and donors</h1>
+        <p>Chari offers a completely new way to raise funds and run fundraising campaigns</p>
       </header>
+
+      <NavLink to="/new/deploy">
+        <Button>Create Your Campaign</Button>
+      </NavLink>
     </div>
   )
 }
