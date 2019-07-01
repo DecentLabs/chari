e15f39e412b96208cb392a5049308a5e6f8528e5
brigittaforrai <forraibrigi@gmail.com>
2019-07-01 14:08:27 +0200

    navigation, logo

--- client/src/components/navigation.js
+++ client/src/components/navigation.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import styles from './../styles/navigation.module.css'
 
+import logo from './../assets/text-logo.svg'
+
 export default function Navigation () {
-  return <div className={styles.navigation}>navigation</div>
+  return (
+    <div className={styles.navigation}>
+      <div className={styles.left}>
+        <img src={logo} alt="chari logo" className={styles.logo}/>
+      </div>
+
+      <div className={styles.right}>
+        <a className={styles.link} to="">Featured campaigns</a>
+        <a className={styles.link} to="">Learn more</a>
+        <a className={styles.link} to="">Contact</a>
+      </div>
+    </div>
+  )
 }
