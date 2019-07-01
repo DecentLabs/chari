5ac50d1318ba55d12f93cbc1a6f3d53c7a887afc
Daniel Fogaras <daniel.fogaras@gmail.com>
2019-07-01 13:26:39 +0200

    sol: rename Fundraiser to DonationMatching

--- contracts/DonationMatching.sol
+++ contracts/DonationMatching.sol
@@ -1,101 +1,77 @@
-pragma solidity ^0.5.1;
+pragma solidity 0.5.1;
 
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/blob/master/contracts/token/ERC20/IERC20.sol";
 
-contract DonationMatching {
-
-    struct Grant {
-        uint amount;
-        uint refundDue;
-        bool tallied;
+contract Fundable {
+    
+    function() external payable {
+        require(msg.data.length == 0); // only allow plain transfers
     }
     
-    address payable public sponsor;
+    function tokenBalance(address token) public view returns (uint) {
+        if (token == address(0x0)) {
+            return address(this).balance;
+        } else {
+            return IERC20(token).balanceOf(address(this));
+        }
+    }
+   
+   function send(address payable destionation, address token, uint amount) internal {
+       if (token == address(0x0)) {
+           destionation.transfer(amount);
+       } else {
+           IERC20(token).transfer(destionation, amount);
+       }
+   }
+}
+
+contract DonationMatching is Fundable {
     address payable public recipient;
     uint public expiration;
-
-    address[] public tokens;
-
-    mapping (address => Grant) public grantsByToken;
+    Grant public grant;
 
     constructor(address payable _recipient, uint _expiration) public {
         require(_expiration > now);
         require(_expiration < now + 365 days);
-        sponsor = msg.sender;
         recipient = _recipient;
         expiration = _expiration;
-    }
-    
-    // accept ETH transfers as donations
-    function() external payable {
-        require(msg.data.length == 0); // only allow plain transfers
+        grant = new Grant(this, msg.sender);
     }
 
-    function sponsorEther() external payable {
-        storeGrant(address(0x0), msg.value);
-    }
-
-    function sponsorTokens(address token, uint amount) external {
-        require(token != address(0x0));
-        storeGrant(token, amount);
-        IERC20(token).transferFrom(msg.sender, address(this), amount);
-    }
-
-    function storeGrant(address token, uint amount) private {
-        require(amount > 0);
-        require(sponsor == msg.sender);
-        require(!hasExpired());
-        Grant storage grant = grantsByToken[token];
-        if (grant.amount == 0) {
-            tokens.push(token);
-        }
-        grant.amount += amount;
-    }
-
-    function tally(address token) private returns (Grant storage) {
-        require(hasExpired());
-        Grant storage grant = grantsByToken[token];
-        if (!grant.tallied) {
-            uint total;
-            if (token == address(0x0)) {
-                total = address(this).balance;   
-            } else {
-                total = IERC20(token).balanceOf(address(this));
-            }
-            uint contrib = total - grant.amount;
-            uint donation = min(contrib * 2, total);
-            grant.refundDue = total - donation;   
-            grant.tallied = true;
-        }
-        return grant;
-    }
-
-    function disburseDonation(address token) external {
-        uint refundDue = tally(token).refundDue;
-        if (token == address(0x0)) {
-            recipient.transfer(address(this).balance - refundDue);
-        } else {
-            uint total = IERC20(token).balanceOf(address(this));
-            IERC20(token).transfer(recipient, total - refundDue);
-        }
-    }
-
-    function refundSponsor(address token) external {
-        Grant storage grant = tally(token);
-        uint refundDue = grant.refundDue;
-        grant.refundDue = 0;
-        if (token == address(0x0)) {
-            sponsor.transfer(refundDue);   
-        } else {
-            IERC20(token).transfer(sponsor, refundDue);
-        }
-    }
-    
     function hasExpired() public view returns (bool) {
         return now >= expiration;
     }
+
+    function disburse(address token) public {
+        require(hasExpired());
+        grant.tally(token);
+        send(recipient, token, tokenBalance(token));
+    }
+}
+
+contract Grant is Fundable {
+    DonationMatching public donationMatching;
+    address payable public sponsor; 
+    mapping (address => bool) public tallied;
     
-    function min(uint a, uint b) private pure returns (uint) {
-        return a < b ? a : b;
+    constructor(DonationMatching _donationMatching, address payable _sponsor) public {
+        donationMatching = _donationMatching;
+        sponsor = _sponsor;
+    }
+    
+    function refund(address token) public {
+        require(donationMatching.hasExpired());
+        tally(token);
+        send(sponsor, token, tokenBalance(token));
+    }
+
+    function tally(address token) public {
+        require(donationMatching.hasExpired());
+        if (!tallied[token]) {
+            uint raised = donationMatching.tokenBalance(token);
+            uint grant = tokenBalance(token);
+            tallied[token] = true;
+            send(address(donationMatching), token, raised > grant ? grant : raised);
+        }
     }
 }
