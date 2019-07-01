ca048a107cccdb938fb5c8e4e084efc46aba8d45
brigittaforrai <forraibrigi@gmail.com>
2019-07-01 17:28:38 +0200

    fonts

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './../styles/Home.module.css'
+
 import {NavLink} from "react-router-dom"
 
 import Button from './../components/button.js'
@@ -8,8 +9,8 @@ export default function Home () {
   return (
     <div className={styles.home}>
       <header>
-        <h1>A new class of potential matching sponsors and donors</h1>
-        <p>Chari offers a completely new way to raise funds and run fundraising campaigns</p>
+        <h1 className="title">A new class of potential <br/>matching sponsors and donors</h1>
+        <p className="big">Chari offers a completely new way to raise funds and run <br/>fundraising campaigns</p>
       </header>
 
       <NavLink to="/new/deploy">
