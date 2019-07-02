63e56473873567a69282da8cf2e6c10ce427fe65
Viktor Szathmáry <phraktle@gmail.com>
2019-07-02 15:25:11 +0200

    contracts: enable solc optimizer

--- truffle-config.js
+++ truffle-config.js
@@ -26,5 +26,15 @@ module.exports = {
       gas: 3000000,
       gasPrice: 10000000000
     }
+  },
+  compilers: {
+    solc: {
+      settings: {
+        optimizer: {
+          enabled: true, 
+          runs: 200    
+        }
+      }
+    }
   }
 };
