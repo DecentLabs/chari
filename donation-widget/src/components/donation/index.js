6545464f2b3ac288a9c11329e4b74e43ff99522f
robi <robert.szaloki@euedge.com>
2019-07-02 18:13:06 +0200

    connects to fundraiser and grant contracts

--- donation-widget/src/components/donation/index.js
+++ donation-widget/src/components/donation/index.js
@@ -1,12 +1,13 @@
 import { h, Component } from 'preact'
 import { donation } from './style.scss'
-import { NODES } from '../../constants.js'
+import { NODES, tokens } from '../../constants.js'
 import Web3 from 'Web3'
 import Fundraiser from '../../../../deployments/Fundraiser.json'
 import Grant from '../../../../deployments/Grant.json'
 
 export default class Donation extends Component {
-  state = {web3: null, fundraiserContract: null}
+  state = {web3: null, fundraiserContract: null, grantContract: null, donations: null, grants:null}
+
   componentDidMount () {
     const network = parseInt(this.props.network, 10)
     const url = NODES[network]
@@ -14,14 +15,23 @@ export default class Donation extends Component {
     const web3 = new Web3(provider)
     const fundraiserAddress = this.props.fundraiser
     const fundraiserContract = new web3.eth.Contract(Fundraiser.abi, fundraiserAddress)
-
+    fundraiserContract.methods.grant().call().then(grantAddress => {
+      const grantContract = new web3.eth.Contract(Grant.abi, grantAddress);
+      this.setState({grantContract})
+    })
     this.setState({web3, fundraiserContract})
   }
 
-  render (props, {fundraiserContract}) {
+  componentDidUpdate() {
+    const {web3, fundraiserContract, grantContract} = this.state
+  }
+
+  render (props, {fundraiserContract, grantContract}) {
     return (
       <div className={donation}>
         <h1>fundraiser contract address: {fundraiserContract && fundraiserContract.address}</h1>
+        <h1>grant contract address: {grantContract && grantContract.address}</h1>
+        <h1></h1>
       </div>
     )
   }
--- donation-widget/src/constants.js
+++ donation-widget/src/constants.js
@@ -1,1 +1,7 @@
-export const NODES = ['https://mainnet.infura.io/v3/', "", "", "", 'https://rinkeby.infura.io/v3/']
+
+export const NODES = ['https://mainnet.infura.io/v3/', "", "", "", 'https://rinkeby.infura.io/v3/0aac55d3d6bb4b3498e97cbcf7112f6b']
+
+export const tokens = [null, null, null, null, {
+  "ETH":'0x0',
+  "AEUR": "0x79065a165Ec09E6A89D584a14872802717FE12a3"
+}]
