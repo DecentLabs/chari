08e74111e98162e13d23e74144525ecf1fb8fc8d
brigittaforrai <forraibrigi@gmail.com>
2019-07-03 17:07:50 +0200

    styles, admin page init

--- client/src/views/WidgetEditor.js
+++ client/src/views/WidgetEditor.js
@@ -1,9 +1,17 @@
 import React from "react"
+import styles from './../styles/widgetEditor.module.css'
 
 function WidgetEditor () {
   return (
-    <div>
-      widget editor
+    <div className={styles.widgetEditor}>
+      <header>
+        <h2>Welcome in campaign editor page!</h2>
+        <p>You can manage your campaign widget from here, customize it and also copy the code necessary to embed it on your website.</p>
+      </header>
+
+      <div className={styles.widget}>
+        <iframe src=""></iframe>
+      </div>
     </div>
   )
 }
