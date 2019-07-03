815cdedb8b9ee92066b423376ea5a31ce50412c0
juli_e <julia.ero@euedge.com>
2019-07-03 14:05:19 +0200

    add datepicker

--- client/src/components/input.js
+++ client/src/components/input.js
@@ -3,11 +3,11 @@ import styles from './../styles/input.module.css'
 
 class Input extends React.Component {
   render () {
-    const {label, placeHolder, name, onChange} = this.props
+    const {label, placeHolder, name, onChange, onClick, value} = this.props
     return (
       <div className={styles.inputWrapper}>
         <label className={styles.label}>{label}</label>
-        <input className={styles.input} name={name} placeholder={placeHolder} onChange={onChange}/>
+        <input className={styles.input} name={name} placeholder={placeHolder} onChange={onChange} onClick={onClick} value={value}/>
       </div>
     )
   }
