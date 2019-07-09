e100f948b8d8409261eff7f6f7d75f3d3b230909
brigittaforrai <forraibrigi@gmail.com>
2019-07-09 14:13:12 +0200

    widget data

--- widget/src/components/donation.js
+++ widget/src/components/donation.js
@@ -20,24 +20,59 @@ export default connect([
   fundraiserContract,
   expiration,
   raised,
-  matched
+  matched,
+  grantBalance
 }) => {
   const token = "ETH"
   const _raised = raised ? raised.find((item) => item.token === token) : null
   const _matched = matched ? matched.find((item) => item.token === token) : null
+  const grant = grantBalance ? grantBalance.find((item) => item.token === token) : null
+
+  let progress = null
+
+  if (_matched && _matched.value && grant && grant.value) {
+    progress = (
+      <div class="progressCont">
+        <p>Sponsor matches each donated tokens.</p>
+        <progress value={_matched.value} max={grant.value}></progress>
+        <p className="progressInfo">{_matched.value} {_matched.token} matched</p>
+        <p>{_matched.value} {_matched.token} from donors & {_matched.value} {_matched.token} from sponsors</p>
+      </div>
+    )
+  } else if (grant && grant.value && !_matched && !_matched.value ) {
+      progress = (
+        <div class="progressCont">
+          <p>Sponsor donated {grant.value} {grant.token} ...todo text.</p>
+          <progress value={grant.value} max={grant.value}></progress>
+          <p>{_matched.value} {_matched.token} matched. Donate to double your money. TODO TEXT</p>
+        </div>
+      )
+  } else {
+    progress = (
+      <div class="progressCont">
+        <p>Campaign is not ready.</p>
+      </div>
+    )
+  }
 
   return (
     <div>
       <Expiration at={expiration}/>
 
-      {_raised && (_raised.value !== null) && (
-        <div class="raised">{_raised.value} {_raised.token}</div>
-      )}
-      {_matched && (_matched.value !== null) && (
-        <div class="matched">{_matched.value} {_matched.token}</div>
-      )}
+      <div class="matchDetails">
+        {progress}
+
+        {_raised && (_raised.value !== null) && (
+          <div class="raisedCont">
+            <div class="raised">{_raised.value} {_raised.token}</div>
+            <p>Raised together</p>
+          </div>
+        )}
+
+      </div>
 
       <hr/>
+
       {fundraiserContract && (
         <Link href={ROUTES.CONTRIBUTION}>DONATE!</Link>
       )}
