e6e8b89007f8d0f4db14d34bbc515f779d072af6
robi <robert.szaloki@euedge.com>
2019-07-10 15:44:07 +0200

    footer

--- shared/constants.js
+++ shared/constants.js
@@ -27,19 +27,22 @@ const DAI_MAIN = {
 NETWORKS.set(1, {
   url: 'https://mainnet.infura.io/v3/0aac55d3d6bb4b3498e97cbcf7112f6b',
   tokens: [ETH, AEUR_MAIN, DAI_MAIN],
-  factory: '0x18662Eef5F00f59A765830B571FAc1d273F4b8D8'
+  factory: '0x18662Eef5F00f59A765830B571FAc1d273F4b8D8',
+  etherscan:'https://etherscan.io/'
 })
 
 NETWORKS.set(3, {
   url: 'https://ropsten.infura.io/v3/0aac55d3d6bb4b3498e97cbcf7112f6b',
   tokens: [ETH],
   factory: '0x1329857cDa42634C501C1d5f2757761Ccde07436',
-  name:'Ropsten testnet'
+  name:'Ropsten testnet',
+  etherscan:'https://ropsten.etherscan.io/'
 })
 
 NETWORKS.set(4, {
   url: 'https://rinkeby.infura.io/v3/0aac55d3d6bb4b3498e97cbcf7112f6b',
   tokens: [ETH, AEUR_RINKEBY],
   factory: '0x43D5e98C1350DB149f24C4cf9a181e4d7b6fC2d2',
-  name:'Rinkeby testnet'
+  name:'Rinkeby testnet',
+  etherscan:'https://rinkeby.etherscan.io/'
 })
