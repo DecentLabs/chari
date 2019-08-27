004350aa1d35ef19e73b2970ed2b7a6c29b53198
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-27 15:58:46 +0000

    Deploy DecentLabs/chari to github.com/DecentLabs/chari.git:gh-pages

---
+++ precache-manifest.e752eb7102319d177fc93da9799c92e1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca57933eedc2cbddc54e",
+    "url": "/static/css/main.4787dd1b.chunk.css"
+  },
+  {
+    "revision": "ca57933eedc2cbddc54e",
+    "url": "/static/js/main.ca57933e.chunk.js"
+  },
+  {
+    "revision": "b8054d1499239b545053",
+    "url": "/static/css/1.bd4448e3.chunk.css"
+  },
+  {
+    "revision": "b8054d1499239b545053",
+    "url": "/static/js/1.b8054d14.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "88645411f33a3a3b3ddf55a09ba83e8b",
+    "url": "/static/media/close-button.88645411.svg"
+  },
+  {
+    "revision": "e96a61b9f9bb5ecd7b37f769a725586b",
+    "url": "/static/media/tick.e96a61b9.svg"
+  },
+  {
+    "revision": "dbc4e82510756c9be09e8f5479d69fb0",
+    "url": "/static/media/home-bg.dbc4e825.svg"
+  },
+  {
+    "revision": "ede7daf07768d536963824cb13a017a7",
+    "url": "/static/media/down-arrow.ede7daf0.svg"
+  },
+  {
+    "revision": "fe7acb786d56261b5e562d0aef48e401",
+    "url": "/static/media/text-logo.fe7acb78.svg"
+  },
+  {
+    "revision": "dd0bc58f05c4dd9449fe171cd40778e2",
+    "url": "/static/media/loader.dd0bc58f.svg"
+  },
+  {
+    "revision": "adcd6733d1ef2e4a9af87d7a27cb1411",
+    "url": "/static/media/medal-2.adcd6733.svg"
+  },
+  {
+    "revision": "272fe54160338152750bed4b0814d893",
+    "url": "/static/media/copy.272fe541.svg"
+  },
+  {
+    "revision": "20394e33061c76dbd4f39cab50ce73d3",
+    "url": "/static/media/bird-01.20394e33.svg"
+  },
+  {
+    "revision": "9a0694abcef750abe9fd2ea3f0327b7b",
+    "url": "/static/media/coin-divider.9a0694ab.svg"
+  },
+  {
+    "revision": "5c09ca95995091a51a5e73290a588317",
+    "url": "/index.html"
+  }
+];
